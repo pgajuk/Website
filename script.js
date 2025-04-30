249b9c836b36078a7f69b20ffeb6da56e17e6f75
pgajuk <gajukpawle@gmail.com>
2025-04-30 18:12:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".phon-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+
+
+
+
+
